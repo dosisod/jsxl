b3ace8b04d462c4a9028a4567bd5e5e96db53465
dosisod <39638017+dosisod@users.noreply.github.com>
2018-11-26 22:24:09 -0800

    created constructor

--- jsxl.js
+++ jsxl.js
@@ -1,12 +1,21 @@
-rows=16
-columns=16
-
 window.onload=function() {
-	document.getElementById("table")
-	tr=document.createElement("tr")
-	for (i of rows) {
-		th=document.createElement("th")
-		th.innerHTML=(i==0?"X":i-1)
-		tr.appendChild(th)
+	xl=new jsxl()
+}
+
+class jsxl {
+	constructor() {
+		this.ROWS=16
+		this.COLS=16
+
+		this.$=Array(this.ROWS).fill(0).map(x=>Array(this.COLS).fill(0)) //initializes the table
+	}
+	init() {
+		document.getElementById("table")
+		tr=document.createElement("tr")
+		for (i of rows) {
+			th=document.createElement("th")
+			th.innerHTML=(i==0?"X":i-1)
+			tr.appendChild(th)
+		}
 	}
 }
