94bb2406b683b433ba63ba5b1539a2b70d753a05
dosisod <39638017+dosisod@users.noreply.github.com>
2018-12-01 23:35:54 -0800

    updated UI

--- jsxl.js
+++ jsxl.js
@@ -15,7 +15,7 @@ class jsxl {
 	init() {
 		//does top header
 		var temptr=document.createElement("tr")
-		for (var i=0;i<=this.COLS+1;i++) {
+		for (var i=0;i<=this.COLS;i++) {
 			var tempth=document.createElement("th")
 			if (i==0) { //if its 0, print an 'x'
 				tempth.innerHTML="x"
@@ -28,6 +28,25 @@ class jsxl {
 			temptr.appendChild(tempth) //append it to table
 		}
 		this.table.appendChild(temptr)
+
+		for (var i=0;i<this.ROWS;i++) { //for every row
+			var temptr=document.createElement("tr") //make a new tr
+			for (var j=0;j<=this.COLS;j++) { //fill each col with input box
+				if (j==0) {
+					var temptd=document.createElement("th")
+					temptd.innerHTML=i
+					temptd.id="R"+i
+				}
+				else {
+					var temptd=document.createElement("td")
+					var input=document.createElement("input")
+					input.id="R"+i+"-C"+j
+					temptd.appendChild(input)
+				}
+				temptr.appendChild(temptd)
+			}
+			this.table.appendChild(temptr)
+		}
 
 		/* old code for creating table, will delete later
 		document.getElementById("table")
